2c09be5eb72b935e8ae9caf94e39180333b5fd19
ilya506 <sajfulin2015@gmail.com>
2023-08-07 13:47:12 +0000

    JS-INITIAL

--- алгоритм/algorithm.js
+++ алгоритм/algorithm.js
@@ -8,3 +8,25 @@ function first(numb){
         return true;
     }
 }
+
+function first(numb){
+    var number = numb;
+    if(Math.floor(number / 5) == number / 5) {
+        //Чётное
+        return false;
+    }else{
+        //Нечётное
+        return true;
+    }
+}
+
+function first(numb){
+    var number = numb;
+    if(Math.floor(number / 8) == number /8) {
+        //Чётное
+        return false;
+    }else{
+        //Нечётное
+        return true;
+    }
+}
